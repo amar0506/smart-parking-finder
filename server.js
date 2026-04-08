2786c7fa0a39596dcd01b8c20ab5e0a7f9f9e13c
Amar Vishwakarma <amarvishwakarma069@gmail.com>
2026-04-08 20:55:18 +0530

    connect frontend

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname, "attached_assets")));
+// Serve frontend files
+app.use(express.static(path.join(__dirname)));
 
+// Main route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "attached_assets", "index.html"));
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
